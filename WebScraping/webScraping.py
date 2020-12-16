281883fdedc52d63bf95530a886805b3e5b8bcdc
natelau926 <57794632+natelau926@users.noreply.github.com>
2020-12-15 22:37:02 -0800

    updated code

--- WebScraping/webScraping.py
+++ WebScraping/webScraping.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
 
 driver = webdriver.Chrome()
 driver.get("")   # link
-for i in range(1,3):
+for i in range(1,35):  # scroll time
     driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
     time.sleep(2)
-class_name = driver.find_elements_by_class_name('covershot-con')
+class_name = driver.find_elements_by_class_name("tile__covershot")
+#class_name = driver.find_elements_by_class_name("covershot-con")
 links = [elem.get_attribute('href') for elem in class_name]
+
 for link in links:
-    print(link)
+    print("ed.auto_offer('" + link + "')")
